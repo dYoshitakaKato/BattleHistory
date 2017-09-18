72c5c27fdea142ec7315dd5673f78d7614fdc997
dYoshitakaKato <phoenix1331019[gmail.com>
2017-09-19 01:35:35 +0900

    DBContextクラスの追加

---
+++ BattleHistory/BattleHistory/BattleHistoryDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleHistory
+{
+    internal class BattleHistoryDBContext : DbContext
+    {
+        public BattleHistoryDBContext() : base("name=LevelLogDBContext")
+        {
+
+        }
+
+        public virtual DbSet<Models.BattleHistory> BattleHistories { get; set; }
+        public virtual DbSet<Models.GetShip> GetShips { get; set; }
+        public virtual DbSet<Models.BattleResult> BattleResults { get; set; }
+        public virtual DbSet<Models.Sortie> Sorties { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
