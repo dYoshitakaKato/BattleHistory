934ec47a62fd82cf5a45d4cf71e4d68c2cc3a881
dYoshitakaKato <phoenix1331019[gmail.com>
2017-09-19 01:34:57 +0900

    BaseHistory Modelの追加

---
+++ BattleHistory/BattleHistory/Models/BattleHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper.Models.Raw;
+
+namespace BattleHistory.Models
+{
+    public class BattleHistory : Sortie
+    {
+        public BattleResult BattleResult { get; set; }
+
+        public BattleHistory(kcsapi_map_start api_map_start) : base(api_map_start)
+        {
+        }
+    }
+}
