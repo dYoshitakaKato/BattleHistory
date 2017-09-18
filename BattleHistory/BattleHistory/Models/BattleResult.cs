b3a42836d7037e2f58f0cc0a3424970ce9619661
dYoshitakaKato <phoenix1331019[gmail.com>
2017-09-18 21:52:22 +0900

    BattleResult Modelの追加

---
+++ BattleHistory/BattleHistory/Models/BattleResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleWrapper.Models.Raw;
+
+namespace BattleHistory.Models
+{
+    class BattleResult : RawDataWrapper<kcsapi_battleresult>
+    {
+        internal BattleResult(kcsapi_battleresult rawData) : base(rawData)
+        {
+            getShip = new GetShip(rawData.api_get_ship);
+        }
+
+        public string WinRank => this.RawData.api_win_rank;
+
+        public GetShip getShip;
+    }
+}
