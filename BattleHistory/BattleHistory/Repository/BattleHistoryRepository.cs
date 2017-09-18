763b0443047569e95a234cf7ff54607132b8ace1
dYoshitakaKato <phoenix1331019[gmail.com>
2017-09-19 01:36:12 +0900

    Repositoryクラスの追加

---
+++ BattleHistory/BattleHistory/Repository/BattleHistoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleHistory.Repository
+{
+    public class BattleHistoryRepository
+    {
+        public bool Insert(Models.BattleHistory model)
+        {
+            using (BattleHistoryDBContext context = new BattleHistoryDBContext())
+            {
+                //context.BattleHistories.
+            }
+            return true;
+        }
+
+        internal int GetSortieCount()
+        {
+            int count = 1;
+            using (BattleHistoryDBContext context = new BattleHistoryDBContext())
+            {
+                count = context.BattleHistories.Select(x => x.SortieCount).Max();
+            }
+            return count;
+        }
+    }
+}
